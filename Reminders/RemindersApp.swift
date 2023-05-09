1ef21ad2b7a25788fbf03d984e2f83b6aaa66987
Imen Ksouri <imen_ksouri@hotmail.fr>
2023-05-09 10:04:36 +0200

    Initial Commit

---
+++ Reminders/RemindersApp.swift
@@ -0,0 +1,20 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by Imen Ksouri on 09/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
